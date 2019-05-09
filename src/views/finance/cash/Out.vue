4fa09f8459f199c2f4c45277e7de3bf536b7885a
Martien Dermawan Tanama <martiendt@gmail.com>
2019-05-09 09:58:49 +0700

    fix cash out

--- src/views/finance/cash/Out.vue
+++ src/views/finance/cash/Out.vue
@@ -2,201 +2,160 @@
   <div>
     <breadcrumb>
       <breadcrumb-finance/>
-      <router-link to="/purchase/order" class="breadcrumb-item">{{ $t('cash') | titlecase }}</router-link>
-      <span class="breadcrumb-item active">Create</span>
+      <span class="breadcrumb-item active">{{ $t('payment order') | titlecase }}</span>
     </breadcrumb>
 
     <tab-menu/>
 
+    <div class="row">
+      <p-block :title="$t('payment order')" :header="true">
+        <div class="row mb-10">
+          <p-date-range-picker
+            id="date"
+            name="date"
+            class="col-sm-4"
+            v-model="date"/>
+        </div>
+        <p-form-input
+          id="search-text"
+          name="search-text"
+          placeholder="Search"
+          :value="searchText"
+          @input="filterSearch"/>
+        <hr>
+        <p-block-inner :is-loading="isLoading">
+          <point-table>
+            <tr slot="p-head">
+              <th>#</th>
+              <th>Number</th>
+              <th>Date</th>
+              <th>Account</th>
+              <th>Notes</th>
+              <th class="text-right">Amount</th>
+              <th></th>
+            </tr>
+            <template v-for="(paymentOrder, index) in paymentOrders">
+              <template v-for="(paymentOrderDetail, index2) in paymentOrder.details">
+              <tr :key="'payment-order-' + index + '-' + index2" slot="p-body">
+                <th>{{ index + 1 + ( ( currentPage - 1 ) * limit ) }}</th>
+                <td>
+                  <router-link :to="{ name: 'finance.payment-order.show', params: { id: paymentOrder.id }}">
+                    {{ paymentOrder.form.number }}
+                  </router-link>
+                </td>
+                <td>{{ paymentOrder.form.date | dateFormat('DD MMMM YYYY HH:mm') }}</td>
+                <td>{{ paymentOrderDetail.account.number }} - {{ paymentOrderDetail.account.alias }}</td>
+                <td>{{ paymentOrderDetail.notes }}</td>
+                <td class="text-right">{{ paymentOrderDetail.amount | numberFormat }}</td>
+                <td>
+                  <router-link class="btn btn-sm btn-secondary mr-5" :to="{ name: 'finance.cash.out', query: { id: paymentOrder.id }}">
+                    <i class="fa fa-share-square-o"></i> Payment
+                  </router-link>
+                </td>
+              </tr>
+              </template>
+            </template>
+          </point-table>
+        </p-block-inner>
+        <p-pagination
+          :current-page="currentPage"
+          :last-page="lastPage"
+          @updatePage="updatePage">
+        </p-pagination>
+      </p-block>
+    </div>
   </div>
 </template>
 
 <script>
-import debounce from 'lodash/debounce'
 import TabMenu from './TabMenu'
 import Breadcrumb from '@/views/Breadcrumb'
 import BreadcrumbFinance from '../Breadcrumb'
-import Form from '@/utils/Form'
+import debounce from 'lodash/debounce'
 import PointTable from 'point-table-vue'
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
   components: {
     TabMenu,
-    PointTable,
     Breadcrumb,
-    BreadcrumbFinance
+    BreadcrumbFinance,
+    PointTable
   },
   data () {
     return {
-      isSaving: false,
-      isLoading: false,
-      form: new Form({
-        increment_group: this.$moment().format('YYYYMM'),
-        date: this.$moment().format('YYYY-MM-DD HH:mm:ss'),
-        supplier_id: null,
-        supplier_name: null,
-        approver_id: null,
-        need_down_payment: 0,
-        cash_only: false,
-        notes: null,
-        subtotal: 0,
-        discount_percent: 0,
-        discount_value: 0,
-        tax_base: 0,
-        tax: 0,
-        type_of_tax: 'exclude',
-        total_quantity: 0,
-        amount: 0,
-        items: [{
-          item_id: null,
-          item_name: null,
-          item: {
-            units: [{
-              label: '',
-              name: '',
-              converter: null
-            }],
-          },
-          unit: null,
-          converter: null,
-          quantity: null,
-          price: null,
-          total: null,
-          allocation_id: null,
-          allocation_name: null,
-          notes: null
-        }]
+      date: {
+        start: this.$route.query.date_from ? this.$moment(this.$route.query.date_from).format('YYYY-MM-DD 00:00:00') : this.$moment().format('YYYY-MM-01 00:00:00'),
+        end: this.$route.query.date_to ? this.$moment(this.$route.query.date_to).format('YYYY-MM-DD 23:59:59') : this.$moment().format('YYYY-MM-DD 23:59:59')
+      },
+      isLoading: true,
+      searchText: this.$route.query.search,
+      currentPage: this.$route.query.page * 1 || 1,
+      lastPage: 1,
+      limit: 10
+    }
+  },
+  watch: {
+    date: function () {
+      this.$router.push({
+        query: {
+          ...this.$route.query,
+          date_from: this.date.start,
+          date_to: this.date.end
+        }
       })
+      this.getPaymentOrders()
     }
   },
   computed: {
-    ...mapGetters('purchaseRequest', ['purchaseRequest']),
-    ...mapGetters('purchaseOrder', ['purchaseOrder'])
+    ...mapGetters('financePaymentOrder', ['paymentOrders', 'pagination'])
   },
   methods: {
-    ...mapActions('purchaseRequest', ['find']),
-    ...mapActions('purchaseOrder', ['create']),
-    addItemRow () {
-      this.form.items.push({
-        item_id: null,
-        item_name: null,
-        item: {
-          units: [{
-            label: '',
-            name: '',
-            converter: null
-          }],
-        },
-        unit: null,
-        converter: null,
-        quantity: null,
-        price: null,
-        allocation_id: null,
-        allocation_name: null,
-        notes: null
-      })
-    },
-    deleteRow (index) {
-      this.$delete(this.form.items, index)
-    },
-    chooseSupplier (value) {
-      this.form.supplier_name = value
-    },
-    chooseItem (item, row) {
-      row.item_name = item.name
-      row.item.units = item.units
-      row.item.units.forEach((unit, keyUnit) => {
-        if (unit.converter == 1) {
-          row.unit = unit.label
-          row.converter = unit.converter
-        }
-      })
-    },
-    chooseAllocation (allocation, row) {
-      row.allocation_name = allocation
-    },
-    chooseTax (taxType) {
-      if (taxType == this.form.type_of_tax) {
-        this.form.type_of_tax = null
-      } else {
-        this.form.type_of_tax = taxType
-      }
-      this.calculate()
-    },
-    calculate: debounce (function () {
-      var subtotal = 0
-      var totalQuantity = 0
-      this.form.items.forEach(function (element) {
-        element.total = element.quantity * (element.price - (element.price * element.discount_percent / 100))
-        element.discount_value = element.discount_percent * element.price / 100
-        subtotal += parseFloat(element.total)
-        totalQuantity += parseFloat(element.quantity)
-      })
-      this.form.subtotal = subtotal
-      this.form.discount_value = this.form.discount_percent * subtotal / 100
-      this.form.total_quantity = totalQuantity
-      this.form.tax_base = this.form.subtotal - (this.form.subtotal * this.form.discount_percent / 100)
-      if (this.form.type_of_tax == 'include') {
-        this.form.tax = this.form.tax_base * 10 / 100
-        this.form.amount = this.form.tax_base
-      } else if (this.form.type_of_tax == 'exclude') {
-        this.form.tax = this.form.tax_base * 10 / 100
-        this.form.amount = this.form.tax_base + this.form.tax
-      } else {
-        this.form.tax = 0
-        this.form.amount = this.form.tax_base
-      }
+    ...mapActions('financePaymentOrder', ['get']),
+    filterSearch: debounce(function (value) {
+      this.$router.push({ query: { search: value } })
+      this.searchText = value
+      this.currentPage = 1
+      this.getPaymentOrders()
     }, 300),
-    onSubmit () {
-      this.isSaving = true
-      this.form.increment_group = this.$moment(this.form.date).format('YYYYMM')
-      if (this.form.approver_id == null) {
-        this.$notification.error('approval cannot be null')
-        this.isSaving = false
-        this.form.errors.record({
-          approver_id: ['Approver should not empty']
-        })
-        return
-      }
-      this.create(this.form)
-        .then(response => {
-          this.isSaving = false
-          this.$notification.success('create success')
-          Object.assign(this.$data, this.$options.data.call(this))
-          this.$router.push('/purchase/order/' + response.data.id)
-        }).catch(error => {
-          this.isSaving = false
-          this.$notification.error(error.message)
-          this.form.errors.record(error.errors)
-        })
-    }
-  },
-  created () {
-    if (this.$route.query.id) {
+    getPaymentOrders () {
       this.isLoading = true
-      this.find({
-        id: this.$route.query.id,
+      this.get({
         params: {
-          includes: 'form;supplier;items.item.units;services.service'
+          join: 'form',
+          sort_by: '-forms.number',
+          fields: 'payment_orders.*',
+          filter_form: 'activePending',
+          filter_like: {
+            'form.number': this.searchText
+          },
+          filter_min: {
+            'form.date': this.serverDateTime(this.$moment(this.date.start).format('YYYY-MM-DD 00:00:00'))
+          },
+          filter_max: {
+            'form.date': this.serverDateTime(this.$moment(this.date.end).format('YYYY-MM-DD 23:59:59'))
+          },
+          limit: this.limit,
+          includes: 'form;details.account;details.allocation',
+          page: this.currentPage
         }
       }).then(response => {
         this.isLoading = false
-        this.form.purchase_request_id = response.data.id
-        this.form.date = response.data.form.date
-        this.form.supplier_id = response.data.supplier_id
-        this.form.supplier_name = response.data.supplier_name
-        this.form.notes = response.data.form.notes
-        this.form.amount = response.data.amount
-        this.form.items = response.data.items
-        this.form.items.forEach(function (element) {
-          element.purchase_request_item_id = element.id
-        })
-        this.calculate()
       }).catch(error => {
         this.isLoading = false
+        this.$notification.error(error.message)
       })
-    }
+    },
+    updatePage (value) {
+      this.currentPage = value
+      this.getPaymentOrders()
+    }    
+  },
+  created () {
+    this.getPaymentOrders()
+  },
+  updated () {
+    this.lastPage = this.pagination.last_page
   }
 }
 </script>
