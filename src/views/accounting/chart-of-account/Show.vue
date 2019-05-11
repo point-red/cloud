9a1ee4b1161af6c5d9b701fe2d64b2a1e10471b1
Martien Dermawan Tanama <martiendt@gmail.com>
2019-05-11 07:50:18 +0700

    fix journal coa

--- src/views/accounting/chart-of-account/Show.vue
+++ src/views/accounting/chart-of-account/Show.vue
@@ -53,41 +53,23 @@
       <div class="col-lg-8">
         <p-block :title="'chart of account'" :header="true">
           <p-block-inner :is-loading="isLoading">
-            <p-form-row
-              id="account-type"
-              name="account-type"
-              :label="$t('account type')">
-              <div slot="body" class="col-lg-9 mt-5">
-                {{ chartOfAccount.type.alias }}
-              </div>
-            </p-form-row>
-
-            <p-form-row
-              id="name"
-              :label="$t('name')"
-              name="name">
-              <div slot="body" class="col-lg-9 mt-5">
-                {{ chartOfAccount.alias }}
-              </div>
-            </p-form-row>
-
-            <p-form-row
-              id="number"
-              :label="$t('number')"
-              name="number">
-              <div slot="body" class="col-lg-9 mt-5">
-                {{ chartOfAccount.number }}
-              </div>
-            </p-form-row>
-
-            <hr>
-
-            <router-link
-              :to="{ path: '/accounting/chart-of-account/' + chartOfAccount.id + '/edit', params: { id: chartOfAccount.id }}"
-              v-if="$permission.has('update chart of account')"
-              class="btn btn-sm btn-primary mr-5">
-              Edit
-            </router-link>
+            <point-table>
+              <tr slot="p-head">
+                <th></th>
+                <th>Notes</th>
+                <th>Debit</th>
+                <th>Credit</th>
+              </tr>
+              <tr
+                slot="p-body"
+                v-for="(journal, index) in journals"
+                :key="index">
+                <th></th>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </point-table>
           </p-block-inner>
         </p-block>
       </div>
@@ -99,13 +81,15 @@
 import TabMenu from './TabMenu'
 import Breadcrumb from '@/views/Breadcrumb'
 import BreadcrumbAccounting from '@/views/accounting/Breadcrumb'
+import PointTable from 'point-table-vue'
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
   components: {
     TabMenu,
     Breadcrumb,
-    BreadcrumbAccounting
+    BreadcrumbAccounting,
+    PointTableVue
   },
   data () {
     return {
@@ -115,9 +99,11 @@ export default {
   },
   computed: {
     ...mapGetters('accountingChartOfAccount', ['chartOfAccount']),
+    ...mapGetters('accountingJournal', ['journals'])
   },
   methods: {
     ...mapActions('accountingChartOfAccount', ['find']),
+    ...mapActions('accountingJournal', ['get'])
   },
   created () {
     this.isLoading = true
@@ -136,6 +122,7 @@ export default {
         this.isLoading = false
         this.$notification.error(error.message)
       })
+    this.get()
   }
 }
 </script>
