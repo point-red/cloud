914daaa1d19298f9f5b117b72fc1ea06d1a119d1
Martien Dermawan Tanama <martiendt@gmail.com>
2019-06-15 09:36:31 +0700

    vue google map

--- src/views/plugin/pin-point/sales-visitation-form/Create.vue
+++ src/views/plugin/pin-point/sales-visitation-form/Create.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <p-loading-block v-show="isLoading" :message="loadingMessage"/>
+
     <breadcrumb>
       <breadcrumb-plugin/>
       <breadcrumb-pin-point/>
@@ -13,7 +15,7 @@
 
     <form class="row" @submit.prevent="onSubmit">
       <p-block
-        :is-loading="loading"
+        :is-loading="isLoading"
         :header="true"
         :title="$t('sales visitation')"
         column="col-sm-12">
@@ -36,43 +38,94 @@
           id="customer"
           name="customer"
           :label="$t('customer')"
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.customer"
           :errors="form.errors.get('customer')"
           @errors="form.errors.set('customer', null)">
         </p-form-row>
 
-        <p-form-row
-          :label="'group'">
+        <p-form-row :label="'group'">
           <div slot="body" class="col-lg-9">
             <p-select
               id="group"
               name="group"
-              :disabled="loadingSaveButton"
+              :disabled="isSaving"
               v-model="form.group"
               :options="groupList"
               :errors="form.errors.get('group')"
               @errors="form.errors.set('group', null)"/>
           </div>
         </p-form-row>
-
+      
         <p-form-row
           id="address"
           name="address"
           :label="$t('address')"
           :placeholder="$t('address')"
-          help="*wajib diisi dan akan diverifikasi, jika alamat tidak ditemukan maka piutang dibebankan pada SA"
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.address"
           :errors="form.errors.get('address')"
           @errors="form.errors.set('address', null)">
+        </p-form-row>
+
+        {{ markers[0] }}
+        {{ addressComponent }}
+        <p-form-row
+          id="gmap"
+          name="gmap"
+          :label="'Google Map - ' + $t('address')"
+          :placeholder="'' + $t('address')">
+          <div slot="body" class="col-lg-9">
+            <gmap-autocomplete :value="description"
+              @place_changed="setPlace"
+              v-on:keypress.enter.prevent
+              :disabled="isSaving"
+              class="form-control">
+            </gmap-autocomplete>
+            <gmap-map
+              id="map"
+              ref="map"
+              :center="center"
+              :zoom="15"
+              :options="{
+                disableDefaultUI: true,
+                styles: [
+                  {
+                    featureType: 'poi.business',
+                    stylers: [
+                      {
+                        visibility: 'off'
+                      }
+                    ]
+                  },
+                  {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text',
+                    stylers: [
+                      {
+                        visibility: 'off'
+                      }
+                    ]
+                  }
+                ]
+              }"
+              style="width: 100%; height: 200px">
+                <gmap-marker
+                  :key="index"
+                  v-for="(m, index) in markers"
+                  :position="center = m.position"
+                  :clickable="true"
+                  :draggable="true"
+                  @click="center=m.position"></gmap-marker>
+            </gmap-map>
+          </div>
         </p-form-row>
 
         <p-form-row
           id="district"
           name="district"
           :placeholder="$t('district')"
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.district"
           :errors="form.errors.get('district')"
           @errors="form.errors.set('district', null)">
@@ -82,7 +135,7 @@
           id="sub_district"
           name="sub_district"
           :placeholder="$t('sub district')"
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.sub_district"
           :errors="form.errors.get('sub_district')"
           @errors="form.errors.set('sub_district', null)">
@@ -92,8 +145,7 @@
           id="phone"
           name="phone"
           :label="$t('phone')"
-          help="*wajib diisi dan akan diverifikasi, jika nomer tidak valid maka piutang dibebankan pada SA"
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.phone"
           :errors="form.errors.get('phone')"
           @errors="form.errors.set('phone', null)">
@@ -109,7 +161,7 @@
               name="similar_product"
               :multiple="true"
               :key-field="'label'"
-              :disabled="loadingSaveButton"
+              :disabled="isSaving"
               v-model="form.similar_product"
               :options="similarProductList"
               :errors="form.errors.get('similar_product')"
@@ -122,7 +174,7 @@
           name="other_similar_product"
           :label="$t('')"
           placeholder="..."
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.other_similar_product"
           :errors="form.errors.get('other_similar_product')"
           @errors="form.errors.set('other_similar_product', null)">
@@ -138,7 +190,7 @@
               name="interest"
               :multiple="true"
               :key-field="'label'"
-              :disabled="loadingSaveButton"
+              :disabled="isSaving"
               v-model="form.interest_reason"
               :options="interestedList"
               :errors="form.errors.get('interest')"
@@ -151,7 +203,7 @@
           name="other_interest_reason"
           :label="$t('')"
           placeholder="..."
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.other_interest_reason"
           :errors="form.errors.get('other_interest_reason')"
           @errors="form.errors.set('other_interest_reason', null)">
@@ -166,7 +218,7 @@
               id="not-interest"
               name="not_interest"
               :multiple="true"
-              :disabled="loadingSaveButton"
+              :disabled="isSaving"
               v-model="form.not_interest_reason"
               :options="notInterestedList"
               :errors="form.errors.get('not_interest')"
@@ -179,7 +231,7 @@
           name="other_not_interest_reason"
           :label="$t('')"
           placeholder="..."
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.other_not_interest_reason"
           :errors="form.errors.get('other_not_interest_reason')"
           @errors="form.errors.set('other_not_interest_reason', null)">
@@ -189,7 +241,7 @@
           id="notes"
           name="notes"
           :label="'notes'"
-          :disabled="loadingSaveButton"
+          :disabled="isSaving"
           v-model="form.notes"
           :errors="form.errors.get('notes')"
           @errors="form.errors.set('notes', null)">
@@ -208,7 +260,7 @@
             <td>
               <p-select
                 id="item"
-                :disabled="loadingSaveButton"
+                :disabled="isSaving"
                 v-model="form.item[index]"
                 @input="updateItem(row)"
                 :options="itemList"
@@ -219,14 +271,14 @@
               <p-form-number
                 v-model="form.quantity[index]"
                 @keyup.native="calculate()"
-                :disabled="loadingSaveButton"
+                :disabled="isSaving"
                 :is-text-right="true"/>
             </td>
             <td>
               <p-form-number
                 v-model="form.price[index]"
                 @keyup.native="calculate()"
-                :disabled="loadingSaveButton"
+                :disabled="isSaving"
                 :is-text-right="true"/>
             </td>
             <td>
@@ -295,7 +347,7 @@
               id="payment-received"
               name="payment_received"
               v-model="form.payment_received"
-              :disabled="loadingSaveButton"
+              :disabled="isSaving"
               :errors="form.errors.get('payment_received')"
               @errors="form.errors.set('payment_received', null)"
               :is-text-right="false"/>
@@ -306,8 +358,8 @@
 
         <div class="form-group row">
           <div class="col-md-12">
-            <button :disabled="loadingSaveButton" type="submit" class="btn btn-sm btn-primary">
-              <i v-show="loadingSaveButton" class="fa fa-asterisk fa-spin"/> Save
+            <button :disabled="isSaving" type="submit" class="btn btn-sm btn-primary">
+              <i v-show="isSaving" class="fa fa-asterisk fa-spin"/> Save
             </button>
           </div>
         </div>
@@ -322,6 +374,7 @@ import TabMenu from './TabMenu'
 import Breadcrumb from '@/views/Breadcrumb'
 import BreadcrumbPlugin from '@/views/plugin/Breadcrumb'
 import BreadcrumbPinPoint from '@/views/plugin/pin-point/Breadcrumb'
+import { loaded } from 'vue2-google-maps'
 import Form from '@/utils/Form'
 import { mapActions } from 'vuex'
 
@@ -334,8 +387,9 @@ export default {
   },
   data () {
     return {
-      loading: false,
-      loadingSaveButton: false,
+      isLoading: false,
+      loadingMessage: 'Loading...',
+      isSaving: false,
       rows: 1,
       form: new Form({
         date: this.$moment().format('YYYY-MM-DD HH:mm:ss'),
@@ -416,11 +470,68 @@ export default {
         { id: 'R008 PREMIUM BIJI SEAL PACK 1KG', label: 'R008 PREMIUM BIJI SEAL PACK 1KG' },
         { id: 'R012 PREMIUM PACK HOREKA 1KG', label: 'R012 PREMIUM PACK HOREKA 1KG' },
         { id: 'Y007 NEW GEN BULK PACK 1KG', label: 'Y007 NEW GEN BULK PACK 1KG' }
-      ]
+      ],
+      // Google Map
+      center: {lat: 23.8103, lng: 90.4125},
+      markers: [
+          {position: {lat: 10.0, lng: 10.0}}
+      ],
+      addressComponent: {},
+      getMap: this.$root.mapping,
+      description: '',
+      latLng: {},
+      place: null
+    }
+  },
+  watch: {
+    // 
+  },
+  mounted () {
+    if (navigator.geolocation) {
+      this.isLoading = true
+      this.loadingMessage = 'Searching current location'
+      navigator.geolocation.getCurrentPosition(function (position) {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        this.center.lat = pos.lat
+        this.center.lng = pos.lng
+        this.markers[0].position.lat = pos.lat
+        this.markers[0].position.lng = pos.lng
+        this.$refs.map.$mapPromise.then(() => {
+          this.isLoading = false
+          this.geocodeLatLng(new google.maps.Geocoder, pos, google.maps.InfoWindow)
+        }).catch(error => {
+          this.isLoading = false
+        })
+      }.bind(this))
     }
   },
   methods: {
     ...mapActions('pluginPinPointSalesVisitationForm', ['create']),
+    // [Start] Google Map
+    setDescription (description) {
+      this.description = description
+    },
+    setPlace (place) {
+      this.latLng = {
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng()
+      }
+    },
+    geocodeLatLng (geocoder, map, infowindow){
+      var self = this
+      geocoder.geocode({'location':this.center}, function (results, status) {
+        console.log(results)
+        if (status == 'OK') {
+          self.addressComponent = results[0]
+          console.log('OK ' + JSON.stringify(self.addressComponent.address_components[0]))
+          self.setDescription(self.addressComponent.formatted_address)
+        }
+      })
+    },
+    // [End] Google Map
     updateItem (row) {
       if (this.rows === row) {
         this.rows++
@@ -439,14 +550,14 @@ export default {
       }
     }, 500),
     onSubmit () {
-      this.loadingSaveButton = true
+      this.isSaving = true
       this.create(this.form)
         .then((response) => {
-          this.loadingSaveButton = false
+          this.isSaving = false
           this.$notification.success('Pengisian form sukses')
           this.$router.push('/')
         }, (error) => {
-          this.loadingSaveButton = false
+          this.isSaving = false
           this.$notification.error(error)
           if (error.errors) {
             this.form.errors.record(error.errors)
