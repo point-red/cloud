9a5d8ffabb86acaab38391d83da25a4b60105ce4
Martien Dermawan Tanama <martiendt@gmail.com>
2019-06-21 15:04:47 +0700

    add cloud storage management

---
+++ src/modules/cloud-storage.js
@@ -0,0 +1,103 @@
+import api from '@/api'
+
+const url = '/storage'
+
+const state = {
+  cloudStorage: {},
+  cloudStorages: [],
+  pagination: {}
+}
+
+const getters = {
+  cloudStorage: state => {
+    return state.cloudStorage
+  },
+  cloudStorages: state => {
+    return state.cloudStorages
+  },
+  pagination: state => {
+    return state.pagination
+  }
+}
+
+const mutations = {
+  'FETCH_ARRAY' (state, payload) {
+    state.cloudStorages = payload.data
+    state.pagination = payload.meta
+  },
+  'FETCH_OBJECT' (state, payload) {
+    state.cloudStorage = payload.data
+  },
+  'CREATE' (state, payload) {
+    state.cloudStorage = payload
+  },
+  'UPDATE' (state, payload) {
+    state.cloudStorage = payload
+  },
+  'DELETE' (state, payload) {
+    state.cloudStorage = {}
+  }
+}
+
+const actions = {
+  get ({ commit }, payload) {
+    return new Promise((resolve, reject) => {
+      api.get(url, payload)
+        .then(response => {
+          commit('FETCH_ARRAY', response)
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  },
+  find ({ commit }, payload) {
+    return new Promise((resolve, reject) => {
+      api.get(url + '/' + payload.id, payload)
+        .then(response => {
+          commit('FETCH_OBJECT', response)
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  },
+  create (context, payload) {
+    return new Promise((resolve, reject) => {
+      api.post(url, payload)
+        .then(response => {
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  },
+  update (context, payload) {
+    return new Promise((resolve, reject) => {
+      api.patch(url + '/' + payload.id, payload)
+        .then(response => {
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  },
+  delete (context, payload) {
+    return new Promise((resolve, reject) => {
+      api.delete(url + '/' + payload.id, payload)
+        .then(response => {
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
--- src/views/Storage.vue
+++ src/views/Storage.vue
@@ -1,5 +1,107 @@
 <template>
+  <div>
+    <breadcrumb>
+      <span class="breadcrumb-item active">{{ $t('storage') | titlecase }}</span>
+    </breadcrumb>
+
+    <div class="row">
+      <p-block>
+        <p-form-input
+          id="search-text"
+          name="search-text"
+          placeholder="Search"
+          ref="searchText"
+          :value="searchText"
+          @input="filterSearch"/>
+
+        <hr/>
+
+        <p-block-inner :is-loading="isLoading">
+          <point-table>
+            <tr slot="p-head">
+              <th>#</th>
+              <th>Project</th>
+              <th>File Name</th>
+              <th>File Expired at</th>
+              <th>Download URL</th>
+            </tr>
+            <tr
+              v-for="(cloudStorage, index) in cloudStorages"
+              :key="cloudStorage.id"
+              slot="p-body">
+              <th>{{ index + 1 }}</th>
+              <td>{{ cloudStorage.project.name }}</td>
+              <td>{{ cloudStorage.file_name }}</td>
+              <td>{{ cloudStorage.expired_at | dateFormat('DD MMM YYYY HH:mm') }}</td>
+              <td><a :href="cloudStorage.download_url">{{ cloudStorage.download_url }}</a></td>
+            </tr>
+          </point-table>
+        </p-block-inner>
+        <p-pagination
+          :current-page="currentPage"
+          :last-page="lastPage"
+          @updatePage="updatePage">
+        </p-pagination>
+      </p-block>
+    </div>
+  </div>
 </template>
 
 <script>
+import Breadcrumb from '@/views/Breadcrumb'
+import PointTable from 'point-table-vue'
+import debounce from 'lodash/debounce'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  components: {
+    Breadcrumb,
+    PointTable
+  },
+  data () {
+    return {
+      isLoading: true,
+      searchText: this.$route.query.search,
+      currentPage: this.$route.query.page * 1 || 1,
+      lastPage: 1
+    }
+  },
+  computed: {
+    ...mapGetters('cloudStorage', ['cloudStorages', 'pagination'])
+  },
+  methods: {
+    ...mapActions('cloudStorage', ['get']),
+    updatePage (value) {
+      this.currentPage = value
+      this.getCloudStorageRequest()
+    },
+    getCloudStorageRequest () {
+      this.isLoading = true
+      this.get({
+        params: {
+          sort_by: 'file_name',
+          limit: 20,
+          page: this.currentPage,
+          file_name: this.searchText
+        }
+      }).then(response => {
+        this.isLoading = false
+      }).catch(error => {
+        this.isLoading = false
+      })
+    },
+    filterSearch: debounce(function (value) {
+      this.$router.push({ query: { search: value } })
+      this.searchText = value
+      this.currentPage = 1
+      this.getCloudStorageRequest()
+    }, 300)
+  },
+  created () {
+    this.getCloudStorageRequest()
+  },
+  updated () {
+    this.lastPage = this.pagination.last_page
+  }
+}
 </script>
