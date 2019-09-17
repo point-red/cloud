eaaf029b12d98ed2e35f97da89744ed5b05e91a4
Martien Dermawan Tanama <martiendt@gmail.com>
2019-09-17 22:58:20 +0700

    fix url

--- src/views/layouts/NotificationDropdown.vue
+++ src/views/layouts/NotificationDropdown.vue
@@ -9,7 +9,7 @@
       <h5 class="h6 text-center py-10 mb-0 border-b text-uppercase">Notifications</h5>
       <ul class="list-unstyled my-20">
         <li v-for="(notification, index) in notifications" :key="index">
-          <a class="text-body-color-dark media mb-15" href="javascript:void(0)">
+          <a class="text-body-color-dark media mb-15" :href="'//'+notification.clickAction">
             <div class="ml-5 mr-15">
               <i class="fa fa-fw fa-info-circle text-primary" v-if="notification.type == 'info'"></i>
               <i class="fa fa-fw fa-warning text-warning" v-if="notification.type == 'warning'"></i>
@@ -67,6 +67,7 @@ export default {
             const data = {
               'type': 'info',
               'message': doc.data().message,
+              'clickAction': doc.data().clickAction,
               'createdAt': doc.data().createdAt
             }
             self.notifications.push(data)
