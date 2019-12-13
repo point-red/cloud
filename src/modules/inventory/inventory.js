d9aa852073bdb86900d71d6d40d20357d75d8eb4
Martien Dermawan Tanama <martiendt@gmail.com>
2019-12-13 16:45:46 +0700

    fix inventory report per item

---
+++ src/modules/inventory/inventory.js
@@ -0,0 +1,101 @@
+import api from '@/api'
+
+const url = function (itemId) {
+  return '/inventory/inventories/' + itemId
+}
+
+const state = {
+  inventory: {},
+  openingBalance: 0,
+  openingBalanceCurrentPage: 0,
+  stockIn: 0,
+  stockInCurrentPage: 0,
+  stockOut: 0,
+  stockOutCurrentPage: 0,
+  endingBalance: 0,
+  inventories: [],
+  pagination: {}
+}
+
+const getters = {
+  inventory: state => {
+    return state.inventory
+  },
+  openingBalance: state => {
+    return state.openingBalance
+  },
+  openingBalanceCurrentPage: state => {
+    return state.openingBalanceCurrentPage
+  },
+  stockIn: state => {
+    return state.stockIn
+  },
+  stockInCurrentPage: state => {
+    return state.stockInCurrentPage
+  },
+  stockOut: state => {
+    return state.stockOut
+  },
+  stockOutCurrentPage: state => {
+    return state.stockOutCurrentPage
+  },
+  endingBalance: state => {
+    return state.endingBalance
+  },
+  inventories: state => {
+    return state.inventories
+  },
+  pagination: state => {
+    return state.pagination
+  }
+}
+
+const mutations = {
+  'FETCH_ARRAY' (state, payload) {
+    state.inventories = payload.data
+    state.openingBalance = payload.opening_balance
+    state.openingBalanceCurrentPage = payload.opening_balance_current_page
+    state.stockIn = payload.stock_in
+    state.stockInCurrentPage = payload.stock_in_current_page
+    state.stockOut = payload.stock_out
+    state.stockOutCurrentPage = payload.stock_out_current_page
+    state.endingBalance = payload.ending_balance
+    state.pagination = payload.meta
+  },
+  'FETCH_OBJECT' (state, payload) {
+    state.inventory = payload.data
+  }
+}
+
+const actions = {
+  get ({ commit }, payload) {
+    return new Promise((resolve, reject) => {
+      api.get(url(payload.itemId), payload)
+        .then(response => {
+          commit('FETCH_ARRAY', response)
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  },
+  find ({ commit }, payload) {
+    return new Promise((resolve, reject) => {
+      api.get(url + '/' + payload.id, payload)
+        .then(response => {
+          commit('FETCH_OBJECT', response)
+          resolve(response)
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
--- src/views/master/item/Show.vue
+++ src/views/master/item/Show.vue
@@ -97,6 +97,53 @@
 
     <div class="row">
       <p-block>
+        <div class="row">
+          <div class="col-sm-4">
+            <p-form-row
+              id="date"
+              name="date"
+              :label="$t('date from')">
+              <div slot="body" class="col-lg-9">
+                <p-date-picker
+                  id="date-from"
+                  name="date_from"
+                  v-model="date_from"/>
+              </div>
+            </p-form-row>
+          </div>
+          <div class="col-sm-4">
+            <p-form-row
+              id="date"
+              name="date"
+              :label="$t('date to')">
+              <div slot="body" class="col-lg-9">
+                <p-date-picker
+                  id="date-to"
+                  name="date_to"
+                  v-model="date_to"/>
+              </div>
+            </p-form-row>
+          </div>
+          <div class="col-sm-4">
+            <p-form-row
+              id="warehouse"
+              name="warehouse"
+              :label="$t('warehouse')">
+              <div slot="body" class="col-lg-9 mt-5">
+                <m-warehouse id="warehouse_id" v-model="warehouseId"/>
+              </div>
+            </p-form-row>
+          </div>
+        </div>
+
+        <p-form-input
+          id="search-text"
+          name="search-text"
+          placeholder="Search"
+          :value="searchText"
+          class="btn-block"
+          @input="filterSearch"/>
+        <hr>
         <p-block-inner :is-loading="isLoading">
           <point-table>
             <tr slot="p-head">
@@ -107,16 +154,31 @@
               <th class="text-right">Quantity</th>
               <th class="text-right">Total Quantity</th>
             </tr>
-            <tr slot="p-body" v-for="(row, index) in item.inventories" :key="index">
+            <tr slot="p-body">
+              <th></th>
+              <td colspan="4" class="text-right"><b>Opening Balance</b></td>
+              <td class="text-right">{{ openingBalanceCurrentPage | numberFormat }}</td>
+            </tr>
+            <tr slot="p-body" v-for="(row, index) in inventories" :key="index">
               <th>{{ index + 1 }}</th>
-              <td>{{ row.form.date | dateFormat }}</td>
+              <td>{{ row.form.date | dateFormat('DD MMMM YYYY HH:mm:ss') }}</td>
               <td>{{ row.form.number }}</td>
               <td>{{ row.warehouse.name }}</td>
               <td class="text-right">{{ row.quantity | numberFormat }}</td>
               <td class="text-right">{{ row.total_quantity | numberFormat }}</td>
             </tr>
+            <tr slot="p-body">
+              <th></th>
+              <td colspan="4" class="text-right"><b>Ending Balance</b></td>
+              <td class="text-right">{{ endingBalance | numberFormat }}</td>
+            </tr>
           </point-table>
         </p-block-inner>
+        <p-pagination
+          :current-page="currentPage"
+          :last-page="lastPage"
+          @updatePage="updatePage">
+        </p-pagination>
       </p-block>
     </div>
   </div>
@@ -127,6 +189,7 @@ import TabMenu from './TabMenu'
 import Breadcrumb from '@/views/Breadcrumb'
 import BreadcrumbMaster from '@/views/master/Breadcrumb'
 import PointTable from 'point-table-vue'
+import debounce from 'lodash/debounce'
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
@@ -141,14 +204,35 @@ export default {
       id: this.$route.params.id,
       title: 'Item',
       isLoading: false,
-      isDeleting: false
+      isDeleting: false,
+      searchText: this.$route.query.search,
+      currentPage: this.$route.query.page * 1 || 1,
+      lastPage: 1,
+      warehouseId: 1,
+      date_from: this.serverDate(),
+      date_to: this.serverDate(),
+      choosen: 'stock-history'
+    }
+  },
+  watch: {
+    'date_from' () {
+      this.$router.push({ query: { search: this.searchText, dateFrom: this.date_from, dateTo: this.date_to } })
+      this.currentPage = 1
+      this.getInventoryRequest()
+    },
+    'date_to' () {
+      this.$router.push({ query: { search: this.searchText, dateFrom: this.date_from, dateTo: this.date_to } })
+      this.currentPage = 1
+      this.getInventoryRequest()
     }
   },
   computed: {
-    ...mapGetters('masterItem', ['item'])
+    ...mapGetters('masterItem', ['item']),
+    ...mapGetters('inventoryInventory', ['inventories', 'pagination', 'openingBalance', 'openingBalanceCurrentPage', 'stockIn', 'stockInCurrentPage', 'stockOut', 'stockOutCurrentPage', 'endingBalance'])
   },
   methods: {
     ...mapActions('masterItem', ['find', 'delete']),
+    ...mapActions('inventoryInventory', ['get']),
     onDelete () {
       this.isDeleting = true
       this.delete({ id: this.id })
@@ -160,21 +244,70 @@ export default {
           this.$notification.error(error.message)
           this.isDeleting = false
         })
+    },
+    getItemRequest () {
+      this.isLoading = true
+      this.find({
+        id: this.id,
+        params: {
+          includes: 'account;units;groups'
+        }
+      }).then(response => {
+        this.isLoading = false
+        this.getInventoryRequest()
+      }).catch(error => {
+        this.isLoading = false
+        this.$notification.error(error.message)
+      })
+    },
+    filterSearch: debounce(function (value) {
+      this.$router.push({ query: { search: value, dateFrom: this.date_from, dateTo: this.date_to } })
+      this.searchText = value
+      this.currentPage = 1
+      this.getInventoryRequest()
+    }, 300),
+    getInventoryRequest () {
+      this.isLoading = true
+      this.get({
+        itemId: this.id,
+        params: {
+          includes: 'form;warehouse',
+          sort_by: 'forms.date',
+          limit: 10,
+          page: this.currentPage,
+          filter_like: {
+            'form.number': this.searchText,
+            'warehouse.name': this.searchText
+          },
+          filter_date_min: {
+            'form.date': this.serverDateTime(this.date_from, 'start')
+          },
+          filter_date_max: {
+            'form.date': this.serverDateTime(this.date_to, 'end')
+          }
+        }
+      }).then(response => {
+        let total = this.openingBalanceCurrentPage
+        this.inventories.forEach(element => {
+          total += element.quantity
+          element.total_quantity = total
+        })
+        this.isLoading = false
+      }).catch(error => {
+        this.isLoading = false
+        this.$notification.error(error.message)
+      })
+    },
+    updatePage (value) {
+      this.currentPage = value
+      this.getInventoryRequest()
     }
   },
   created () {
-    this.isLoading = true
-    this.find({
-      id: this.id,
-      params: {
-        includes: 'account;units;groups;inventories.form;inventories.warehouse'
-      }
-    }).then(response => {
-      this.isLoading = false
-    }).catch(error => {
-      this.isLoading = false
-      this.$notification.error(error.message)
-    })
+    this.getItemRequest()
+  },
+  updated () {
+    this.lastPage = this.pagination.last_page
   }
 }
 </script>
