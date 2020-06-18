fb754b1ffcec0fdfbc4fc2702c41b4ab2e13f6a2
Martien Dermawan Tanama <martiendt@gmail.com>
2020-06-18 08:15:59 +0700

    update package

---
+++ src/views/sales/report/routes.js
@@ -0,0 +1,4 @@
+import SalesReportIndex from '@/views/sales/report/Index'
+export default [
+  { path: '/sales/report', name: 'sales.report.index', component: SalesReportIndex }
+]
--- src/views/sales/routes.js
+++ src/views/sales/routes.js
@@ -1,9 +1,11 @@
 import Sales from '@/views/sales/Index'
 import SalesQuotation from './quotation/routes'
 import SalesOrder from './order/routes'
+import SalesReport from './report/routes'
 
 export default [
   { path: '/sales', name: 'Sales', component: Sales },
   ...SalesQuotation,
-  ...SalesOrder
+  ...SalesOrder,
+  ...SalesReport
 ]
