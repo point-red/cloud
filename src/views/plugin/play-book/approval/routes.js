d63305ad178bee80a6baab8d3a7d0310f4d0e204
Martien Dermawan Tanama <martiendt@gmail.com>
2020-06-27 15:38:55 +0700

    fix route name duplicate

--- src/views/plugin/play-book/approval/routes.js
+++ src/views/plugin/play-book/approval/routes.js
@@ -4,8 +4,8 @@ import Instruction from './Instruction'
 import InstructionSend from './InstructionSend'
 
 export default [
-  { path: '/plugin/play-book/approval/procedure', name: 'Procedure', component: Procedure },
+  { path: '/plugin/play-book/approval/procedure', name: 'ProcedureApproval', component: Procedure },
   { path: '/plugin/play-book/approval/procedure/send', name: 'ProcedureSend', component: ProcedureSend },
-  { path: '/plugin/play-book/approval/instruction', name: 'Instruction', component: Instruction },
+  { path: '/plugin/play-book/approval/instruction', name: 'InstructionApproval', component: Instruction },
   { path: '/plugin/play-book/approval/instruction/send', name: 'InstructionSend', component: InstructionSend }
 ]
