b0e3a0a799f9708aa23b9ae588f4c496c6e22642
Hendrix Silaen <silaen.hjr@gmail.com>
2021-11-25 09:00:42 +0700

    fix print sales invoice

--- src/views/sales/invoice/PrintSalesInvoice.vue
+++ src/views/sales/invoice/PrintSalesInvoice.vue
@@ -5,161 +5,205 @@
     overlay-theme="dark"
     @close="onClose()"
   >
-    <div
+    <table
       id="print-sales-invoice"
       class="print-sales-invoice-container m-2 mb-4 mx-auto"
     >
-      <div
-        class="d-flex justify-content-between"
-        style="margin-bottom: 10px;"
-      >
-        <div style="width: 110px; height: 110px; align-self: center;">
-          <img
-            :src="logoUrl"
-            alt="Logo"
-            style="width: 100px; height: 100px;"
-          >
-        </div>
-        <div class="company-detail">
-          <h1 style="margin-top: 0; margin-bottom: 5px;">
-            Invoice
-          </h1>
-          <h3
-            class="my-5px"
-            style="line-height: 22px"
-          >
-            {{ tenantName }}
-          </h3>
-          <p
-            class="my-5px"
-            style="line-height: 15px;"
-          >
-            {{ tenantAddress }}
-          </p>
-          <p class="my-5px">
-            {{ tenantPhone }}
-          </p>
-        </div>
-      </div>
-      <hr style="height:10px; border:none; color:gray; background-color:gray;">
-      <div
-        class="d-flex justify-content-between"
-        style="margin-top: 10px;"
-      >
-        <table style="margin-right: 5px;">
-          <tr>
-            <td>Date</td>
-            <td>:</td>
-            <td>{{ invoice.date | dateFormat('DD MMMM YYYY') }}</td>
-          </tr>
-          <tr>
-            <td>Invoice number</td>
-            <td>:</td>
-            <td>{{ invoice.form.number }}</td>
-          </tr>
-        </table>
-        <table style="margin-left: 5px;">
-          <tr>
-            <td>Customer</td>
-            <td>:</td>
-            <td>{{ invoice.customer.name || '-' }}</td>
-          </tr>
-          <tr>
-            <td>Address</td>
-            <td>:</td>
-            <td>{{ invoice.customer.address || '-' }}</td>
-          </tr>
-          <tr>
-            <td>Phone number</td>
-            <td>:</td>
-            <td>{{ invoice.customer.phone || '-' }}</td>
-          </tr>
-        </table>
-      </div>
-      <table
-        class="table-items"
-        style="width: 100%;"
-      >
+      <thead>
         <tr>
-          <th>No</th>
-          <th>Item</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th>Discount</th>
-          <th>Total</th>
+          <td>
+            <div
+              class="d-flex justify-content-between"
+              style="margin-bottom: 10px;"
+            >
+              <div style="width: 110px; height: 110px; align-self: center;">
+                <img
+                  :src="logoUrl"
+                  alt="Logo"
+                  style="width: 100px; height: 100px;"
+                >
+              </div>
+              <div class="company-detail">
+                <h1 style="margin-top: 0; margin-bottom: 5px;">
+                  Invoice
+                </h1>
+                <h3
+                  class="my-5px"
+                  style="line-height: 22px"
+                >
+                  {{ tenantName }}
+                </h3>
+                <p
+                  class="my-5px"
+                  style="line-height: 15px;"
+                >
+                  {{ tenantAddress }}
+                </p>
+                <p class="my-5px">
+                  {{ tenantPhone }}
+                </p>
+              </div>
+            </div>
+            <hr class="header-divider">
+            <div
+              class="d-flex justify-content-between"
+              style="margin-top: 10px; margin-bottom: 10px"
+            >
+              <table
+                class="header-detail"
+                style="margin-right: 20px;"
+              >
+                <tr>
+                  <td>Date</td>
+                  <td>:</td>
+                  <td>{{ invoice.date | dateFormat('DD MMMM YYYY') }}</td>
+                </tr>
+                <tr>
+                  <td>Invoice number</td>
+                  <td>:</td>
+                  <td>{{ invoice.form.number }}</td>
+                </tr>
+                <tr v-if="invoice.referenceableType === 'SalesDeliveryNote'">
+                  <td>Sales order number</td>
+                  <td>:</td>
+                  <td>{{ invoice.salesDeliveryNote.deliveryOrder.salesOrder.form.number }}</td>
+                </tr>
+              </table>
+              <table
+                class="header-detail"
+                style="margin-left: 20px;"
+              >
+                <tr>
+                  <td>Customer</td>
+                  <td>:</td>
+                  <td>{{ invoice.customer.name || '-' }}</td>
+                </tr>
+                <tr>
+                  <td>Address</td>
+                  <td>:</td>
+                  <td>{{ invoice.customer.address || '-' }}</td>
+                </tr>
+                <tr>
+                  <td>Phone number</td>
+                  <td>:</td>
+                  <td>{{ invoice.customer.phone || '-' }}</td>
+                </tr>
+              </table>
+            </div>
+          </td>
         </tr>
-        <tr
-          v-for="(row, index) in invoice.items"
-          :key="index"
-        >
-          <td>{{ index+1 }}</td>
-          <td>{{ row.itemName }}</td>
-          <td class="text-right">
-            {{ row.quantity | numberFormat }} {{ row.unit }}
-          </td>
-          <td class="text-right">
-            {{ row.price | numberFormat }}
-          </td>
-          <td class="text-right">
-            {{ row.discountValue | numberFormat }}
-          </td>
-          <td class="text-left">
-            {{ row.quantity * (row.price - row.discountValue) | numberFormat }}
-          </td>
-        </tr>
+      </thead>
+      <tbody>
         <tr>
-          <td
-            colspan="5"
-            class="text-right"
-          >
-            Sub total
+          <td>
+            <table
+              class="table-items"
+              style="width: 100%;"
+            >
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Item</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                  <th>Discount</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tr
+                v-for="(row, index) in invoice.items"
+                :key="index"
+              >
+                <td>{{ index+1 }}</td>
+                <td>{{ row.itemName }}</td>
+                <td class="text-right">
+                  {{ row.quantity | numberFormat }} {{ row.unit }}
+                </td>
+                <td class="text-right">
+                  {{ row.price | numberFormat }}
+                </td>
+                <td class="text-right">
+                  {{ row.discountValue | numberFormat }}
+                </td>
+                <td class="text-left">
+                  {{ row.quantity * (row.price - row.discountValue) | numberFormat }}
+                </td>
+              </tr>
+              <tr>
+                <td
+                  colspan="5"
+                  class="text-right"
+                >
+                  Sub total
+                </td>
+                <td>{{ $attrs.subtotal | numberFormat }}</td>
+              </tr>
+              <tr>
+                <td
+                  colspan="5"
+                  class="text-right"
+                >
+                  Discount
+                </td>
+                <td>{{ invoice.discountValue | numberFormat }}</td>
+              </tr>
+              <tr>
+                <td
+                  colspan="5"
+                  class="text-right"
+                >
+                  Tax base
+                </td>
+                <td>{{ invoice.amount - invoice.tax | numberFormat }}</td>
+              </tr>
+              <tr>
+                <td
+                  colspan="5"
+                  class="text-right"
+                >
+                  Tax
+                </td>
+                <td>{{ invoice.tax | numberFormat }}</td>
+              </tr>
+              <tr>
+                <td
+                  colspan="5"
+                  class="text-right"
+                >
+                  Total
+                </td>
+                <td>{{ invoice.amount | numberFormat }}</td>
+              </tr>
+            </table>
+            <span>Notes: {{ notes }}</span>
+            <div
+              class="d-flex justify-content-end"
+              style="margin-top: 75px;"
+            >
+              <div
+                class="text-center"
+                style="margin-right: 75px;"
+              >
+                <h3>Created by</h3>
+                <br><br><br>
+                {{ invoice.form.createdByUser.fullName }}
+              </div>
+              <div class="text-center">
+                <h3>Approved by</h3>
+                <br><br><br>
+                {{ invoice.form.requestApprovalToUser.fullName }}
+              </div>
+            </div>
           </td>
-          <td>{{ $attrs.subtotal | numberFormat }}</td>
         </tr>
-        <tr>
-          <td
-            colspan="5"
-            class="text-right"
-          >
-            Discount
-          </td>
-          <td>{{ invoice.discountValue | numberFormat }}</td>
-        </tr>
-        <tr>
-          <td
-            colspan="5"
-            class="text-right"
-          >
-            Tax base
-          </td>
-          <td>{{ invoice.amount - invoice.tax | numberFormat }}</td>
-        </tr>
-        <tr>
-          <td
-            colspan="5"
-            class="text-right"
-          >
-            Tax
-          </td>
-          <td>{{ invoice.tax | numberFormat }}</td>
-        </tr>
-        <tr>
-          <td
-            colspan="5"
-            class="text-right"
-          >
-            Total
-          </td>
-          <td>{{ invoice.amount | numberFormat }}</td>
-        </tr>
-      </table>
-      <span>Notes: {{ notes }}</span>
-    </div>
+      </tbody>
+      <tfoot />
+    </table>
     <div class="pull-right">
       <button
         v-print="'print-sales-invoice'"
         type="button"
-        class="btn btn-sm btn-outline-secondary"
+        class="btn btn-sm btn-outline-secondary mt-3"
       >
         {{ $t('print') | uppercase }}
       </button>
@@ -314,5 +358,19 @@ table.table-items, .table-items th, .table-items td {
 .company-detail {
   min-width: 200px;
   max-width: 250px;
+}
+.header-divider {
+  height:10px;
+  border:none;
+  color:gray;
+  background-color:gray;
+}
+@media print {
+  table.header-detail tr td {
+    line-height: 0.5px;
+  }
+  #print-sales-invoice thead tr td {
+    padding-top: 20px;
+  }
 }
 </style>
