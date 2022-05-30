877837a03c8ba18f1c516b303dc9eb0ecc28ca1d
Martien Dermawan Tanama <martiendt@gmail.com>
2022-05-31 06:41:17 +0700

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off',
     'handle-callback-err': 'off',
     eqeqeq: 'off'
   },
