8b6e773990ab92247266d9028930b6994016112b
Martien Dermawan Tanama <martiendt@gmail.com>
2022-06-03 10:29:16 +0700

    lint fix

--- src/views/human-resource/kpi-assessment/Index.vue
+++ src/views/human-resource/kpi-assessment/Index.vue
@@ -108,7 +108,7 @@ export default {
       page: this.$route.query.page * 1 || 1,
       lastPage: 1,
       isAdvanceFilter: false,
-      checkedRow: [],
+      checkedRow: []
     }
   },
   computed: {
