ccee082bd1a01c4ceeb5c816e802bb20f02f2f93
mdzakied <mdzakied123@gmail.com>
2022-11-10 09:50:10 +0700

    Fix Hit API Apporve Delete By Email

--- src/modules/finance/payment.js
+++ src/modules/finance/payment.js
@@ -163,7 +163,7 @@ const actions = {
   approveByEmail (context, payload) {
     return new Promise((resolve, reject) => {
       api
-        .post(url + '/approve', payload)
+        .post(url + '/' + payload.id + '/cancellation-approve', payload)
         .then((response) => {
           resolve(response)
         })
@@ -176,7 +176,7 @@ const actions = {
   rejectByEmail (context, payload) {
     return new Promise((resolve, reject) => {
       api
-        .post(url + '/reject', payload)
+        .post(url + '/' + payload.id + '/cancellation-reject', payload)
         .then((response) => {
           resolve(response)
         })
