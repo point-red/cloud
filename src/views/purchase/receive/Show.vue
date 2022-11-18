a54d50ea1d2c455a3334054c9c40f2aa90579ba4
phangiaphat982015 <phangiaphat982015@gmail.com>
2022-11-18 21:49:17 +0700

    feature/printPurchase: add PRINT button, add route print, add empty component Print

--- src/views/purchase/receive/Show.vue
+++ src/views/purchase/receive/Show.vue
@@ -2,13 +2,12 @@
   <div>
     <breadcrumb v-if="purchaseReceive">
       <breadcrumb-purchase />
-      <router-link
-        to="/purchase/receive"
-        class="breadcrumb-item"
-      >
-        {{ $t('purchase receive') | uppercase }}
+      <router-link to="/purchase/receive" class="breadcrumb-item">
+        {{ $t("purchase receive") | uppercase }}
       </router-link>
-      <span class="breadcrumb-item active">{{ purchaseReceive.form.number | uppercase }}</span>
+      <span class="breadcrumb-item active">{{
+        purchaseReceive.form.number | uppercase
+      }}</span>
     </breadcrumb>
 
     <purchase-menu />
@@ -16,36 +15,46 @@
     <p-show-form-cancellation-status
       :is-loading="isLoading"
       :cancellation-status="purchaseReceive.form.cancellation_status"
-      :cancellation-approval-reason="purchaseReceive.form.cancellation_approval_reason"
-      :request-cancellation-reason="purchaseReceive.form.request_cancellation_reason"
+      :cancellation-approval-reason="
+        purchaseReceive.form.cancellation_approval_reason
+      "
+      :request-cancellation-reason="
+        purchaseReceive.form.request_cancellation_reason
+      "
       @onCancellationApprove="onCancellationApprove"
       @onCancellationReject="onCancellationReject"
     />
 
-    <div
-      v-if="purchaseReceive"
-      class="row"
-    >
+    <div v-if="purchaseReceive" class="row">
       <p-block>
         <p-block-inner :is-loading="isLoading">
           <div class="row">
             <div class="col-sm-12">
               <div class="text-right">
                 <router-link
+                  :to="{
+                    name: 'purchase.receive.print',
+                    params: { id: purchaseReceive.id },
+                  }"
+                  class="btn btn-sm btn-outline-secondary mr-5"
+                >
+                  {{ $t("print") | uppercase }}
+                </router-link>
+                <router-link
                   :to="{ name: 'purchase.receive.create' }"
                   class="btn btn-sm btn-outline-secondary mr-5"
                 >
-                  {{ $t('create') | uppercase }}
+                  {{ $t("create") | uppercase }}
                 </router-link>
-                <!-- <router-link :to="{ name: 'purchase.receive.edit', params: { id: purchaseReceive.id }}" class="btn btn-sm btn-outline-secondary mr-5">
-                  {{ $t('edit') | uppercase }}
-                </router-link> -->
                 <button
-                  v-if="purchaseReceive.form.cancellation_status == null || purchaseReceive.form.cancellation_status == -1"
+                  v-if="
+                    purchaseReceive.form.cancellation_status == null ||
+                    purchaseReceive.form.cancellation_status == -1
+                  "
                   class="btn btn-sm btn-outline-secondary mr-5"
                   @click="$refs.formRequestDelete.open()"
                 >
-                  {{ $t('delete') | uppercase }}
+                  {{ $t("delete") | uppercase }}
                 </button>
                 <m-form-request-delete
                   ref="formRequestDelete"
@@ -54,47 +63,46 @@
               </div>
             </div>
           </div>
-          <hr>
+          <hr />
           <div class="row">
             <div class="col-sm-6">
-              <h4>{{ $t('purchase receive') | uppercase }}</h4>
+              <h4>{{ $t("purchase receive") | uppercase }}</h4>
               <table class="table table-sm table-bordered">
                 <tr>
-                  <td
-                    width="150px"
-                    class="font-weight-bold"
-                  >
-                    {{ $t('form number') | uppercase }}
+                  <td width="150px" class="font-weight-bold">
+                    {{ $t("form number") | uppercase }}
                   </td>
                   <td>{{ purchaseReceive.form.number }}</td>
                 </tr>
                 <tr>
                   <td class="font-weight-bold">
-                    {{ $t('date') | uppercase }}
+                    {{ $t("date") | uppercase }}
                   </td>
-                  <td>{{ purchaseReceive.date | dateFormat('DD MMMM YYYY') }}</td>
+                  <td>
+                    {{ purchaseReceive.date | dateFormat("DD MMMM YYYY") }}
+                  </td>
                 </tr>
                 <tr v-if="purchaseReceive.purchase_order">
                   <td class="font-weight-bold">
-                    {{ $t('reference') | uppercase }}
+                    {{ $t("reference") | uppercase }}
                   </td>
                   <td>{{ purchaseReceive.purchase_order.form.number }}</td>
                 </tr>
                 <tr v-if="purchaseReceive.driver">
                   <td class="font-weight-bold">
-                    {{ $t('warehouse') | uppercase }}
+                    {{ $t("warehouse") | uppercase }}
                   </td>
                   <td>{{ purchaseReceive.warehouse.name }}</td>
                 </tr>
                 <tr v-if="purchaseReceive.driver">
                   <td class="font-weight-bold">
-                    {{ $t('driver') | uppercase }}
+                    {{ $t("driver") | uppercase }}
                   </td>
                   <td>{{ purchaseReceive.driver }}</td>
                 </tr>
                 <tr v-if="purchaseReceive.license_plate">
                   <td class="font-weight-bold">
-                    {{ $t('license plate') | uppercase }}
+                    {{ $t("license plate") | uppercase }}
                   </td>
                   <td>{{ purchaseReceive.license_plate }}</td>
                 </tr>
@@ -106,36 +114,37 @@
               </h6>
               <template v-if="purchaseReceive.form.branch">
                 {{ purchaseReceive.form.branch.address | uppercase }}
-                <br v-if="purchaseReceive.form.branch.phone">{{ purchaseReceive.form.branch.phone | uppercase }}
+                <br v-if="purchaseReceive.form.branch.phone" />{{
+                  purchaseReceive.form.branch.phone | uppercase
+                }}
               </template>
               <h6 class="mt-30 mb-5">
-                {{ $t('supplier') | uppercase }}
+                {{ $t("supplier") | uppercase }}
               </h6>
               {{ purchaseReceive.supplier_name | uppercase }}
-              <div style="font-size:12px">
+              <div style="font-size: 12px">
                 {{ purchaseReceive.supplier_address | uppercase }}
-                <br v-if="purchaseReceive.supplier_phone">{{ purchaseReceive.supplier_phone }}
-                <br v-if="purchaseReceive.supplier_email">{{ purchaseReceive.supplier_email | uppercase }}
+                <br v-if="purchaseReceive.supplier_phone" />{{
+                  purchaseReceive.supplier_phone
+                }}
+                <br v-if="purchaseReceive.supplier_email" />{{
+                  purchaseReceive.supplier_email | uppercase
+                }}
               </div>
             </div>
           </div>
-          <hr>
+          <hr />
           <point-table class="mt-20">
             <tr slot="p-head">
-              <th class="text-center">
-                #
-              </th>
-              <th>{{ $t('item') | uppercase }}</th>
+              <th class="text-center">#</th>
+              <th>{{ $t("item") | uppercase }}</th>
               <th class="text-right">
-                {{ $t('quantity') | uppercase }}
+                {{ $t("quantity") | uppercase }}
               </th>
               <th width="50px" />
             </tr>
             <template v-for="(row, index) in purchaseReceive.items">
-              <tr
-                slot="p-body"
-                :key="index"
-              >
+              <tr slot="p-body" :key="index">
                 <th class="text-center">
                   {{ index + 1 }}
                 </th>
@@ -145,7 +154,7 @@
                     (PID: {{ row.production_number }})
                   </template>
                   <template v-if="row.item.require_expiry_date">
-                    (E/D: {{ row.expiry_date | dateFormat('DD MMMM YYYY') }})
+                    (E/D: {{ row.expiry_date | dateFormat("DD MMMM YYYY") }})
                   </template>
                 </td>
                 <td class="text-right">
@@ -158,22 +167,19 @@
           <div class="row mt-50">
             <div class="col-sm-9">
               <h6 class="mb-0">
-                {{ $t('notes') | uppercase }}
+                {{ $t("notes") | uppercase }}
               </h6>
-              <div style="white-space: pre-wrap;">
+              <div style="white-space: pre-wrap">
                 {{ purchaseReceive.form.notes }}
               </div>
               <div class="d-sm-block d-md-none mt-10" />
             </div>
             <div class="col-sm-3 text-center">
               <h6 class="mb-0">
-                {{ $t('created by') | uppercase }}
+                {{ $t("created by") | uppercase }}
               </h6>
-              <div
-                class="mb-50"
-                style="font-size:11px"
-              >
-                {{ purchaseReceive.form.date | dateFormat('DD MMMM YYYY') }}
+              <div class="mb-50" style="font-size: 11px">
+                {{ purchaseReceive.form.date | dateFormat("DD MMMM YYYY") }}
               </div>
               {{ purchaseReceive.form.created_by.full_name | uppercase }}
               <div class="d-sm-block d-md-none mt-10" />
@@ -183,115 +189,121 @@
       </p-block>
     </div>
 
-    <m-form-request-delete
-      ref="formRequestDelete"
-      @delete="onDelete($event)"
-    />
+    <m-form-request-delete ref="formRequestDelete" @delete="onDelete($event)" />
   </div>
 </template>
 
 <script>
-import PurchaseMenu from '../Menu'
-import Breadcrumb from '@/views/Breadcrumb'
-import BreadcrumbPurchase from '../Breadcrumb'
-import PointTable from 'point-table-vue'
-import { mapGetters, mapActions } from 'vuex'
+import PurchaseMenu from "../Menu";
+import Breadcrumb from "@/views/Breadcrumb";
+import BreadcrumbPurchase from "../Breadcrumb";
+import PointTable from "point-table-vue";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-  name: 'Show',
+  name: "Show",
   components: {
     PurchaseMenu,
     Breadcrumb,
     BreadcrumbPurchase,
-    PointTable
+    PointTable,
   },
-  data () {
+  data() {
     return {
       id: this.$route.params.id,
       isLoading: false,
-      isDeleting: false
-    }
+      isDeleting: false,
+    };
   },
   computed: {
-    ...mapGetters('purchaseReceive', ['purchaseReceive']),
-    ...mapGetters('auth', ['authUser'])
+    ...mapGetters("purchaseReceive", ["purchaseReceive"]),
+    ...mapGetters("auth", ["authUser"]),
   },
   watch: {
-    '$route' (to, from) {
+    $route(to, from) {
       if (to.params.id != from.params.id) {
-        this.id = to.params.id
-        this.purchaseReceiveRequest()
+        this.id = to.params.id;
+        this.purchaseReceiveRequest();
       }
-    }
+    },
   },
-  created () {
-    this.purchaseReceiveRequest()
+  created() {
+    this.purchaseReceiveRequest();
   },
   methods: {
-    ...mapActions('purchaseReceive', {
-      find: 'find',
-      delete: 'delete',
-      cancellationApprove: 'cancellationApprove',
-      cancellationReject: 'cancellationReject'
+    ...mapActions("purchaseReceive", {
+      find: "find",
+      delete: "delete",
+      cancellationApprove: "cancellationApprove",
+      cancellationReject: "cancellationReject",
     }),
-    purchaseReceiveRequest () {
-      this.isLoading = true
+    purchaseReceiveRequest() {
+      this.isLoading = true;
       this.find({
         id: this.id,
         params: {
           with_archives: true,
-          includes: 'supplier;' +
-            'warehouse;' +
-            'items.item.units;' +
-            'purchaseOrder.form;' +
-            'form.createdBy;' +
-            'form.branch'
-        }
-      }).then(response => {
-        this.isLoading = false
-      }).catch(error => {
-        this.isLoading = false
-        this.$notification.error(error.message)
+          includes:
+            "supplier;" +
+            "warehouse;" +
+            "items.item.units;" +
+            "purchaseOrder.form;" +
+            "form.createdBy;" +
+            "form.branch",
+        },
       })
+        .then((response) => {
+          this.isLoading = false;
+        })
+        .catch((error) => {
+          this.isLoading = false;
+          this.$notification.error(error.message);
+        });
     },
-    onDelete (reason) {
-      this.isDeleting = true
+    onDelete(reason) {
+      this.isDeleting = true;
       this.delete({
         id: this.id,
         data: {
-          reason: reason
-        }
-      }).then(response => {
-        this.isDeleting = false
-        this.$notification.success('cancel success')
-        this.purchaseReceiveRequest()
-      }).catch(error => {
-        this.isDeleting = false
-        this.$notification.error(error.message)
-        this.form.errors.record(error.errors)
+          reason: reason,
+        },
       })
+        .then((response) => {
+          this.isDeleting = false;
+          this.$notification.success("cancel success");
+          this.purchaseReceiveRequest();
+        })
+        .catch((error) => {
+          this.isDeleting = false;
+          this.$notification.error(error.message);
+          this.form.errors.record(error.errors);
+        });
     },
-    onCancellationApprove () {
+    onCancellationApprove() {
       this.cancellationApprove({
-        id: this.id
-      }).then(response => {
-        this.$notification.success('cancellation approved')
-        this.$router.push('/purchase/receive')
-      }).catch(error => {
-        this.$notification.error(error.message)
+        id: this.id,
       })
+        .then((response) => {
+          this.$notification.success("cancellation approved");
+          this.$router.push("/purchase/receive");
+        })
+        .catch((error) => {
+          this.$notification.error(error.message);
+        });
     },
-    onCancellationReject (reason) {
+    onCancellationReject(reason) {
       this.cancellationReject({
         id: this.id,
-        reason: reason
-      }).then(response => {
-        this.$notification.success('cancellation rejected')
-        this.purchaseReceiveRequest()
-      }).catch(error => {
-        this.$notification.error(error.message)
+        reason: reason,
       })
-    }
-  }
-}
+        .then((response) => {
+          this.$notification.success("cancellation rejected");
+          this.purchaseReceiveRequest();
+        })
+        .catch((error) => {
+          this.$notification.error(error.message);
+        });
+    },
+  },
+};
 </script>
--- src/views/purchase/receive/routes.js
+++ src/views/purchase/receive/routes.js
@@ -2,5 +2,6 @@ export default [
   { path: '/purchase/receive', name: 'purchase.receive.index', component: () => import('./Index') },
   { path: '/purchase/receive/create', name: 'purchase.receive.create', component: () => import('./Create') },
   { path: '/purchase/receive/:id', name: 'purchase.receive.show', component: () => import('./Show') },
-  { path: '/purchase/receive/:id/edit', name: 'purchase.receive.edit', component: () => import('./Edit') }
+  { path: '/purchase/receive/:id/edit', name: 'purchase.receive.edit', component: () => import('./Edit') },
+  { path: '/purchase/receive/:id/print', name: 'purchase.receive.print', component: () => import('./Print') }
 ]
