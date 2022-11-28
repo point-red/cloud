c95ea7bbd9cd1ba3024cc9465f23af5418a97d21
phangiaphat982015 <phangiaphat982015@gmail.com>
2022-11-28 22:08:59 +0700

    feature/approveDelete: add test case - delete receive and see notification

---
+++ tests/e2e/specs/approveDeletePurchaseReceive.js
@@ -0,0 +1,40 @@
+const { should } = require('chai')
+const url = '/human-resource/employee-group'
+const apiUrl = Cypress.env('VUE_APP_API_ENDPOINT') + '/human-resource/employee/groups'
+const getList = apiUrl + '**'
+const getDetail = apiUrl + '/*'
+const timeOut = { timeout: 300000 }
+
+describe('Approve Deletion Purchase Receive', () => {
+  beforeEach(() => {
+    cy.hideXHR()
+    cy.login('admin', 'admin')
+  })
+
+  it('Find the receive not canceled and delete it', () => {
+    cy.interceptToken()
+    cy.visit('purchase/receive/')
+    cy.waitToken()
+
+    cy.waitVisible('#main-container > .content')
+
+    cy.waitVisible('tbody > tr')
+    cy.get('tbody > tr')
+      .find('td')
+      .each(($element) => {
+        cy.wrap($element).invoke('text').then(text => {
+          if (text.includes('PENDING')) {
+            cy.wrap($element).siblings('th').children('a').click()
+            cy.waitVisible('#main-container > .content')
+          }
+        })
+      })
+    cy.get('.text-right').find('button').contains(' DELETE ').should('be.visible').click({ force: true })
+    cy.get('div.sweet-modal.theme-light.has-title.has-content.is-mobile-fullscreen.is-visible textarea')
+      .should('be.visible')
+      .type('test reason delete receive')
+    cy.get('div.sweet-modal.theme-light.has-title.has-content.is-mobile-fullscreen.is-visible button[type="button"]')
+      .should('be.visible')
+      .click()
+  })
+})
