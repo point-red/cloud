64de25d061fba6fb1c4e5a8649eb5a26301b09cb
phangiaphat982015 <phangiaphat982015@gmail.com>
2022-12-09 22:39:01 +0700

    add test case

--- tests/e2e/specs/purchaseReceive/IndexPurchaseReceive.js
+++ tests/e2e/specs/purchaseReceive/IndexPurchaseReceive.js
@@ -1,12 +1,12 @@
 import { generateText, createPurchaseReceive } from './functions'
 
-const { should } = require("chai")
+const { should } = require('chai')
 const randomString = generateText(7)
 const url = '/purchase/receive'
 const apiUrl = Cypress.env('VUE_APP_API_ENDPOINT') + '/purchase/receives'
 const getList = apiUrl + '**'
 const getDetail = apiUrl + '/*'
-const timeOut = { timeout : 300000 }
+const timeOut = { timeout: 300000 }
 
 describe('Purchase - Purchase receive', () => {
   beforeEach(() => {
@@ -19,13 +19,13 @@ describe('Purchase - Purchase receive', () => {
     cy.visit('/')
     cy.waitToken()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > .row > div > div > a').contains('MAIN MENU').click()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > .row > div > div > a').contains('PURCHASE').should('be.visible').click()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > div > div > a').contains('PURCHASE RECEIVE').should('be.visible').click()
 
     cy.intercept('GET', getList).as('getPurchaseReceive')
@@ -33,18 +33,17 @@ describe('Purchase - Purchase receive', () => {
     cy.wait(2000)
     cy.wait('@getPurchaseReceive', timeOut).its('response.statusCode').should('equal', 200)
 
-    cy.waitVisible('table');
+    cy.waitVisible('table')
     cy.get('table th').should('contain', 'Number')
-    .should('contain', 'Date')
-    .should('contain', 'Supplier')
-    .should('contain', 'Warehouse')
-    .should('contain', 'Item')
-    .should('contain', 'Notes')
-    .should('contain', 'Quantity')
-    .should('contain', 'Price')
-    .should('contain', 'Value')
-    .should('contain', 'Form Status')
-
+      .should('contain', 'Date')
+      .should('contain', 'Supplier')
+      .should('contain', 'Warehouse')
+      .should('contain', 'Item')
+      .should('contain', 'Notes')
+      .should('contain', 'Quantity')
+      .should('contain', 'Price')
+      .should('contain', 'Value')
+      .should('contain', 'Form Status')
   })
 
   it('search purchase receive', () => {
@@ -52,35 +51,33 @@ describe('Purchase - Purchase receive', () => {
     cy.visit('/purchase/receive')
     cy.waitToken()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > div > div > div > div > div > div > a').contains('ADVANCE FILTER').should('be.visible').click()
 
     cy.get('#main-container > .content > div > div > div > div > div > div .row label').should('contain', 'DATE START')
-    .should('contain', 'DATE END')
-    .should('contain', 'FORM STATUS')
+      .should('contain', 'DATE END')
+      .should('contain', 'FORM STATUS')
 
     cy.get('#main-container > .content > div > div > div > div > div > div .row span').should('contain', 'SELECT').click()
     cy.get('.sweet-title', timeOut).should('contain', 'SELECT FORM STATUS')
     cy.get('.sweet-content-content .list-group a').should('contain', 'DONE').should('contain', 'PENDING').should('contain', 'CANCELED')
     cy.get('.sweet-content-content .list-group a').contains('DONE').click()
 
-    cy.waitVisible('table');
+    cy.waitVisible('table')
     cy.get('table td').should('contain', 'DONE')
 
     cy.get('#main-container > .content > div > div > div > div > div > div .row span').should('contain', 'DONE').click()
     cy.get('.sweet-title', timeOut).should('contain', 'SELECT FORM STATUS')
     cy.get('.sweet-content-content .list-group a').contains('PENDING').click()
 
-    cy.waitVisible('table');
+    cy.waitVisible('table')
     cy.get('table td').should('contain', 'PENDING')
 
     cy.get('#main-container > .content > div > div > div > div > div > div .row span').should('contain', 'PENDING').click()
     cy.get('.sweet-title', timeOut).should('contain', 'SELECT FORM STATUS')
     cy.get('.sweet-content-content .list-group a').contains('CANCELED').click()
 
-    cy.waitVisible('table');
+    cy.waitVisible('table')
     cy.get('table td').should('contain', 'CANCELED')
-
   })
 })
-  
--- tests/e2e/specs/purchaseReceive/editPurchaseReceive.js
+++ tests/e2e/specs/purchaseReceive/editPurchaseReceive.js
@@ -1,12 +1,12 @@
 import { generateText, createPurchaseReceive } from './functions'
 
-const { should } = require("chai")
+const { should } = require('chai')
 const randomString = generateText(7)
 const url = '/purchase/receive'
 const apiUrl = Cypress.env('VUE_APP_API_ENDPOINT') + '/purchase/receives'
 const getList = apiUrl + '**'
 const getDetail = apiUrl + '/*'
-const timeOut = { timeout : 300000 }
+const timeOut = { timeout: 300000 }
 
 describe('Purchase - Purchase receive', () => {
   beforeEach(() => {
@@ -19,22 +19,19 @@ describe('Purchase - Purchase receive', () => {
     cy.visit('/purchase/receive')
     cy.waitToken()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > div > div > div > div > div > div > a[href="/purchase/receive/create"]').should('have.class', 'input-group-prepend').should('be.visible').click()
-
   })
 
   it('Able to see edit purchase receive form', () => {
     cy.interceptToken()
-    cy.visit('/purchase/receive')
     cy.waitToken()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > div > div > div > div > div > div > a[href="/purchase/receive/create"]').should('have.class', 'input-group-prepend').should('be.visible').click()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > div > form').contains('PURCHASE RECEIVE').should('be.visible')
-
   })
 
   it('Submit edit purchase receive form', () => {
@@ -42,13 +39,12 @@ describe('Purchase - Purchase receive', () => {
     cy.visit('/purchase/receive')
     cy.waitToken()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > div > div > div > div > div > div > a[href="/purchase/receive/create"]').should('have.class', 'input-group-prepend').should('be.visible').click()
 
-    cy.waitVisible('#main-container > .content');
+    cy.waitVisible('#main-container > .content')
     cy.get('#main-container > .content > div > form').contains('PURCHASE RECEIVE').should('be.visible')
 
     cy.get('#main-container > .content > div > form button').contains('SAVE').should('be.visible').click()
   })
 })
-  
