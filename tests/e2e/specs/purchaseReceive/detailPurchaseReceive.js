fe9a65206cc8a9b229c816cb47d32b8342d3fadc
phangiaphat982015 <phangiaphat982015@gmail.com>
2022-12-12 00:15:23 +0700

    add test case

---
+++ tests/e2e/specs/purchaseReceive/detailPurchaseReceive.js
@@ -0,0 +1,28 @@
+import { generateText, createPurchaseReceive } from './functions'
+
+const { should } = require('chai')
+const randomString = generateText(7)
+const url = '/purchase/receive'
+const apiUrl = Cypress.env('VUE_APP_API_ENDPOINT') + '/human-resource/employee/groups'
+const getList = apiUrl + '**'
+const getDetail = apiUrl + '/*'
+const timeOut = { timeout: 300000 }
+
+describe('Purchase - Purchase receive', () => {
+  beforeEach(() => {
+    cy.hideXHR()
+    cy.login('admin', 'admin')
+  })
+
+  it('Click create purchase receive form will be able to select purchase order', () => {
+    cy.interceptToken()
+    cy.visit('/purchase/receive')
+    cy.waitToken()
+
+    cy.waitVisible('#main-container > .content')
+    cy.get('#main-container > .content > div > div > div > div > div > div > a[href="/purchase/receive/create"]').should('have.class', 'input-group-prepend').should('be.visible').click()
+    cy.waitVisible('#main-container > .content')
+    cy.get('#main-container > .content > div > form').contains('PURCHASE RECEIVE').should('be.visible')
+    cy.get('table.table.table-sm.table-bordered').find('tr').contains('  PURCHASE ORDER  ').should('be.visible').find('td').contains(' SELECT ').should('be.visible').click({ force: true })
+  })
+})
