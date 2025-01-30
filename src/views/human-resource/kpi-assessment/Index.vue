f707c92c673cbdaf3dde74ab23d513c38793029c
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-30 07:37:30 +0700

    x

--- src/views/human-resource/kpi-assessment/Index.vue
+++ src/views/human-resource/kpi-assessment/Index.vue
@@ -8,7 +8,7 @@
 
     <div class="row">
       <p-block>
-        <div class="row mb-5" style="margin-left: 12px">
+        <div class="row mb-5" style="margin-left: 0px">
           <div class="">
             <p-select
               id="employee_id"
@@ -18,7 +18,7 @@
               label="employee"
             />
           </div>
-          <div style="margin-left: 12px">
+          <div style="margin-left: 4px">
             <p-date-picker
               id="date"
               v-model="date.start"
@@ -26,7 +26,7 @@
               label="date from"
             />
           </div>
-          <div style="margin-left: 12px">
+          <div style="margin-left: 4px">
             <p-date-picker
               id="date"
               v-model="date.end"
@@ -34,7 +34,7 @@
               label="date to"
             />
           </div>
-          <div style="margin-left: 12px">
+          <div style="margin-left: 4px">
             <button
               type="button"
               class="btn btn-alt-secondary"
