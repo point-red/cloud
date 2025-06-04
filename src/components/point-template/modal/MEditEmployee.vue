a0693a46cb8c86f22726a8f035570198d596fbc2
Martien Dermawan Tanama <martiendt@gmail.com>
2025-06-04 10:24:18 +0700

    x

--- src/components/point-template/modal/MEditEmployee.vue
+++ src/components/point-template/modal/MEditEmployee.vue
@@ -878,7 +878,12 @@ export default {
       }, (error) => {
         console.log(JSON.stringify(error))
       })
-    this.getUser()
+    this.getUser({
+        params: {
+          sort_by: 'name',
+          limit: 5000
+        }
+      })
       .then((response) => {
         const array = []
         response.data.forEach(element => {
